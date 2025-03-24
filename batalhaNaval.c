01d33547d927291b104e323e7373e468660faa6c
Geovani Pereira <111367544+Geper08@users.noreply.github.com>
2025-03-24 14:19:58 +0000

    ajustepara melhor entendimento do codigo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,36 +3,34 @@
 #define TAMANHO 10
 #define NAVIO 3
 
-void inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
+int main() {
+    int tabuleiro[TAMANHO][TAMANHO];
+    char letras[] = "ABCDEFGHIJ";
+
+    // Inicializa o tabuleiro com água (0)
     for (int i = 0; i < TAMANHO; i++) {
         for (int j = 0; j < TAMANHO; j++) {
-            tabuleiro[i][j] = 0; // 0 representa água
+            tabuleiro[i][j] = 0;
         }
     }
-}
 
-void posicionarNavio(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcao) {
+    // Posiciona os navios (valores fixos)
+    int linhaH = 2, colunaH = 3; // Navio horizontal
+    int linhaV = 5, colunaV = 6; // Navio vertical
+
     for (int i = 0; i < NAVIO; i++) {
-        if (direcao == 0) {
-            // Posiciona na horizontal
-            tabuleiro[linha][coluna + i] = NAVIO;
-        } else {
-            // Posiciona na vertical
-            tabuleiro[linha + i][coluna] = NAVIO;
-        }
+        tabuleiro[linhaH][colunaH + i] = NAVIO;
+        tabuleiro[linhaV + i][colunaV] = NAVIO;
     }
-}
 
-void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
-    char letras[] = "ABCDEFGHIJ";
-    
+    // Exibe o tabuleiro
     printf("\nTABULEIRO BATALHA NAVAL\n\n");
     printf("   ");
     for (int i = 0; i < TAMANHO; i++) {
         printf("%c ", letras[i]);
     }
     printf("\n");
-    
+
     for (int i = 0; i < TAMANHO; i++) {
         printf("%2d ", i + 1);
         for (int j = 0; j < TAMANHO; j++) {
@@ -44,36 +42,6 @@ void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
         }
         printf("\n");
     }
-}
 
-int validarPosicao(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcao) {
-    for (int i = 0; i < NAVIO; i++) {
-        if (direcao == 0) {
-            if (coluna + i >= TAMANHO || tabuleiro[linha][coluna + i] != 0) return 0;
-        } else {
-            if (linha + i >= TAMANHO || tabuleiro[linha + i][coluna] != 0) return 0;
-        }
-    }
-    return 1;
-}
-
-int main() {
-    int tabuleiro[TAMANHO][TAMANHO];
-    inicializarTabuleiro(tabuleiro);
-    
-    // Definição das coordenadas iniciais
-    int linhaH = 2, colunaH = 3; // Navio horizontal
-    int linhaV = 5, colunaV = 6; // Navio vertical
-    
-    if (validarPosicao(tabuleiro, linhaH, colunaH, 0)) {
-        posicionarNavio(tabuleiro, linhaH, colunaH, 0);
-    }
-    
-    if (validarPosicao(tabuleiro, linhaV, colunaV, 1)) {
-        posicionarNavio(tabuleiro, linhaV, colunaV, 1);
-    }
-    
-    exibirTabuleiro(tabuleiro);
-    
     return 0;
 }
